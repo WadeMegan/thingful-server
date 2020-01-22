91b2884a92081b35a437716e70e98ef7795b2a04
wademegan96@gmail.com <meganwade@Megans-MacBook-Pro.local>
2020-01-22 16:54:43 -0600

    refactored for bcrypt

--- src/middleware/basic-auth.js
+++ src/middleware/basic-auth.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs')
+const AuthService = require('../auth/auth-service')
+
 function requireAuth(req,res,next){
     const authToken = req.get('Authorization') || ''
     
@@ -19,15 +22,22 @@ function requireAuth(req,res,next){
         return res.status(401).json({error:'Unauthorized request'})
     }
 
-    req.app.get('db')('thingful_users')
-        .where({user_name:tokenUserName})
-        .first()
+    AuthService.getUserWithUserName(
+        req.app.get('db'),
+        tokenUserName
+    )
         .then(user=>{
-            if(!user || user.password !==tokenPassword){
-                return res.status(401).json({error:'Unauthorized request'})
+            if(!user){
+                return res.status(401).json({error: 'Unauthorized request'})
             }
-            req.user = user
-            next()
+            return bcrypt.compare(tokenPassword, user.password)
+                .then(passwordsMatch => {
+                    if(!passwordsMatch){
+                        return res.status(401).json({error:'Unauthorized request'})
+                    }
+                    req.user = user
+                    next()
+                })
         })
         .catch(next)
 }
